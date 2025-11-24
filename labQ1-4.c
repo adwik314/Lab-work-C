b8683b8a200e172a90a605c7f43c10d6e9e8ecbc
adwik314 <adwik314@gmail.com>
2025-11-24 19:02:21 +0530

    Create labQ1-4.c

---
+++ labQ1-4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+    float a, b, Sum ;
+//TAKING INPUT FROM THE USER
+  printf("enter 1st Number\n");
+  scanf("%f", &a);
+ 
+printf("enter 2nd Number\n");
+scanf("%f" , &b);
+
+printf("Sum = %f" , a + b);
+return 0;
+
+}
